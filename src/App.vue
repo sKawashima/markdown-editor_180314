d2a28c80cad3761ea15180c1dbb998f6e080d9f7
Shota Kawashima <bysk.333@gmail.com>
2018-03-14 16:18:54 +0900

    つくった

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template lang='pug'>
+#app
+  textarea(v-model='input')
+  #preview(v-html='output')
+</template>
+
+<script>
+import marked from 'marked'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      input: '',
+      output: ''
+    }
+  },
+  watch: {
+    input: function () {
+      this.output = marked(this.input)
+    }
+  }
+}
+</script>
+
+<style lang='sass'>
+html, body
+  height: 100%
+  width: 100%
+  margin: 0
+
+#app
+  display: flex
+  height: 100%
+
+textarea, #preview
+  flex: 1 1 0
+  overflow-y: scroll
+
+textarea
+  padding: 1em
+  font-size: 12px
+  background-color: #eee
+  line-height: 1.5em
+  font-family: 'Hack', courier, monospace
+  border: 0
+
+#preview
+  padding: 1em
+
+@media print
+  textarea
+    display: none
+  #preview
+    overflow-y: visible
+
+</style>
